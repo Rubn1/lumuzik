d917a0b18067b0b36cef9a4d98686c9ab5fdde93
RUBN <mercyrubn@gmail.com>
2024-10-17 17:19:03 +0200

    update lumuzik

---
+++ lib/core/configs/theme/App_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xff42C83C);
+  static const blackbackground = Color(0xff1E1E1E);
+  static const colorPrincipale = Color(0xFF8C00);
+  static const lightBackground = Color(0xffF2F2F2);
+}
